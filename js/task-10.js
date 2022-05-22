bafc32a950a7e5fb9a0c0bfe9b471351afc8343e
Vitalii <sv.legion8@gmail.com>
2022-05-23 00:41:04 +0300

    2.1

--- js/task-10.js
+++ js/task-10.js
@@ -1,35 +1,31 @@
 const refs = {
-  inputElement: document.querySelector("#controls"),
+  inputEl: document.querySelector("#controls input"),
   createBtn: document.querySelector("[data-create]"),
   destroyBtn: document.querySelector("[data-destroy]"),
-  boxesElement: document.querySelector("#boxes"),
+  boxesEl: document.querySelector("#boxes"),
 };
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// function createBoxes(amount) {
-//   amount = refs.inputElement.value;
-//   const box = [];
-//   for (let i = 0; i < amount; i += 1) {
-//     const div = document.createElement("div");
-//   }
-// }
-// refs.createBtn.addEventListener("click", createBoxes);
+refs.createBtn.addEventListener("click", () =>
+  createBoxes(refs.inputEl.value));
 
+function createBoxes(amount) {
+  const boxStart = 30;
+  
+  for (let i = 0; i < amount; i += 1) {
+    const boxEl = document.createElement("div");
+    boxEl.style.width = boxStart + i * 10 + "px";
+    boxEl.style.height = boxStart + i * 10 + "px";
+    boxEl.style.backgroundColor = getRandomHexColor();
+    
+    refs.boxesEl.appendChild(boxEl);
+  }
+}
 
-// Напиши скрипт создания и очистки коллекции элементов. 
-// Пользователь вводит количество элементов в input и нажимает кнопку
-// Создать, после чего рендерится коллекция.При нажатии на кнопку Очистить,
-//   коллекция элементов очищается.
-
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. 
-// Функция создает столько < div >, сколько указано в amount и добавляет их в div#boxes.
-
-// Размеры самого первого <div> - 30px на 30px.
-// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX. 
-
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes,
-//   тем самым удаляя все созданные элементы.
+refs.destroyBtn.addEventListener("click", function destroyBoxes() {
+  refs.boxesEl.innerHTML = "";
+  refs.inputEl.value = 0;
+});
