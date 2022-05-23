f39b7ef6c5b8373b93b2cbe6ab72c35359559542
Vitalii <sv.legion8@gmail.com>
2022-05-23 17:23:31 +0300

    2.2

--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,5 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
-};
+// Задание 9
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
 
 const bodyColor = document.querySelector("body");
 const btnColor = document.querySelector(".change-color");
@@ -10,5 +9,11 @@ const changeBtnColor = () => {
   bodyColor.style.backgroundColor = getRandomHexColor();
   colorName.textContent = bodyColor.style.backgroundColor
 };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
 btnColor.addEventListener("click", changeBtnColor);
--- js/task-10.js
+++ js/task-10.js
@@ -1,13 +1,19 @@
+// Задание 10 
+// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+// Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
+
+// 1. Размеры самого первого <div> - 30px на 30px.
+// 2. Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+// 3. Все элементы должены иметь случайный цвет фона в формате HEX. 
+
+// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+
 const refs = {
   inputEl: document.querySelector("#controls input"),
   createBtn: document.querySelector("[data-create]"),
   destroyBtn: document.querySelector("[data-destroy]"),
   boxesEl: document.querySelector("#boxes"),
 };
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 
 refs.createBtn.addEventListener("click", () =>
   createBoxes(refs.inputEl.value));
@@ -20,9 +26,14 @@ function createBoxes(amount) {
     boxEl.style.width = boxStart + i * 10 + "px";
     boxEl.style.height = boxStart + i * 10 + "px";
     boxEl.style.backgroundColor = getRandomHexColor();
-    
     refs.boxesEl.appendChild(boxEl);
   }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 refs.destroyBtn.addEventListener("click", function destroyBoxes() {
